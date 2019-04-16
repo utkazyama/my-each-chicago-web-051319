384a075e712853e976105f0f0d56572cc33f02ad
Yuta Katsuyama <ykatsuyama@id.iit.edu>
2019-04-16 04:39:52 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
   # code here
+  i=0 
+  collect=[]
+  while i<array.length
+  collect << yield(array[i])
+  i+=1
+end
+array
 end
